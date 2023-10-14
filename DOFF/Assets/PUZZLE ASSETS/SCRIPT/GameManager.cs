b77cae907e7afe71e39ba0bf5372ae442426e1f2
JECP2 <joshuaerickael.pancrudo.c@bulsu.edu.ph>
2023-10-14 12:48:40 +0800

    up

--- DOFF/Assets/PUZZLE ASSETS/SCRIPT/GameManager.cs
+++ DOFF/Assets/PUZZLE ASSETS/SCRIPT/GameManager.cs
@@ -7,16 +7,20 @@ public class GameManager : MonoBehaviour
     [SerializeField] private Transform gameTransfrom;
     [SerializeField] private Transform piecePrefab;
 
+    private List<Transform> pieces;
     private int emptyLocation;
     private int size;
+    private bool shuffling = false;
 
     private void CreateGamePieces(float gapThickness)
     {
         float width = 1 / (float)size;
-        for (int row = 0; row < size; row++){
-            for (int col = 0; col < size; col++){
+        for (int row = 0; row < size; row++)
+        {
+            for (int col = 0; col < size; col++)
+            {
                 Transform piece = Instantiate(piecePrefab, gameTransfrom);
-                
+                pieces.Add(piece);
                 piece.localPosition = new Vector3(-1 + (2 * width * col) + width,
                                                   +1 - (2 * width * row) - width,
                                                    0);
@@ -48,7 +52,102 @@ public class GameManager : MonoBehaviour
 
     void Start()
     {
+        pieces = new List<Transform>();
         size = 3;
         CreateGamePieces(0.01f);
+    }
+
+
+    void Update()
+    {
+        if (!shuffling && CheckCompletion())
+        {
+            shuffling = true;
+            StartCoroutine(WaitShuffle(0.5f));
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+            if (hit)
+            {
+                for (int i = 0; i < pieces.Count; i++)
+                {
+                    if (pieces[i] == hit.transform)
+                    {
+                        if (SwapIfValid(i, -size, size)) { break; }
+                        if (SwapIfValid(i, +size, size)) { break; }
+                        if (SwapIfValid(i, -1, 0)) { break; }
+                        if (SwapIfValid(i, +1, size)) { break; }
+                    }
+                }
+            }
+        }
+    }
+
+
+
+    private bool SwapIfValid(int i, int offset, int colCheck)
+    {
+        if (((i % size) != colCheck) && ((i + offset) == emptyLocation))
+        {
+            (pieces[i], pieces[i + offset]) = (pieces[i + offset], pieces[i]);
+
+            (pieces[i].localPosition, pieces[i + offset].localPosition) = ((pieces[i + offset].localPosition, pieces[i].localPosition));
+
+            emptyLocation = i;
+            return true;
+        }
+        return false;
+    }
+
+    
+
+    private bool CheckCompletion()
+    {
+        for (int i = 0; i < pieces.Count; i++)
+        {
+            if (pieces[i].name != $"{i}")
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private IEnumerator WaitShuffle(float duration)
+    {
+        yield return new WaitForSeconds(duration);
+        Shuffle();
+        shuffling = false;
+    }
+
+    private void Shuffle()
+    {
+        int count = 0;
+        int last = 0;
+        while (count < (size * size * size))
+        {
+            int rnd = Random.Range(0, size * size);
+
+            if (rnd == last) { continue; }
+            last = emptyLocation;
+            if (SwapIfValid(rnd, -size, size))
+            {
+                count++;
+            }
+            else if (SwapIfValid(rnd, +size, size))
+            {
+                count++;
+            }
+            else if (SwapIfValid(rnd, -1, 0))
+            {
+                count++;
+            }
+            else if (SwapIfValid(rnd, +1, size - 1))
+            {
+                count++;
+            }
+        }
     }
 }
